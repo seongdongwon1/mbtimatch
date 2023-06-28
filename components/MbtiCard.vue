6e923d3a83b7b4298f98facecbb62312310ff49e
SD <sdtv9999@gmail.com>
2023-06-28 16:09:52 +0900

    [improvement] main bg color & card filp

--- components/MbtiCard.vue
+++ components/MbtiCard.vue
@@ -1,6 +1,17 @@
 <template>
   <div :class="['mbti-item', `item${idx}`]">
-    asd
+    <div
+      class="card-inner"
+      :class="{ flipped: isFlipped }"
+      @click="flipCard"
+    >
+      <div class="card-front">
+        I
+      </div>
+      <div class="card-back">
+        E
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +19,19 @@
 export default {
     name: 'MbtiCard',
     props: ['idx'],
-    mounted () {
-
+    data () {
+        return {
+            isFlipped: false
+        }
+    },
+    methods: {
+        flipCard () {
+            this.isFlipped = !this.isFlipped
+        }
     }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    @import '~assets/css/intro.scss';
 </style>
